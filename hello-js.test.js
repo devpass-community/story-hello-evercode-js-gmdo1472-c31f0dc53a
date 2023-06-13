56e7d2c61830b4fc3459d703f30cec350613922f
gabrielmanoel-hub <gabrielmanoel1401@outlook.com>
2023-06-13 17:30:10 -0300

    ../

--- hello-js.test.js
+++ hello-js.test.js
@@ -1,8 +1,7 @@
-const hello = require('../hello')
+const hello = require('./hello')
 
 it('text string', () => {
     const name = 'Evercode'
     const result = hello(name)
     expect(result).toBe('Hello, Evercode!')
 })
-
