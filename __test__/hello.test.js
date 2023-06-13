f0b07a6a735fbb7e66e47cffc5b1e6d0ec86f785
gabrielmanoel-hub <gabrielmanoel1401@outlook.com>
2023-06-13 16:19:17 -0300

    inserindo pontuação gráfica

--- __test__/hello.test.js
+++ __test__/hello.test.js
@@ -1,7 +1,7 @@
 const helloMessage = require('../hello')
 
 it('text string', () => {
-    const name = 'Evercode'
-    expect(helloMessage(name)).toEqual('Hello  Evercode')
+    const name = 'Evercode!'
+    expect(helloMessage(name)).toEqual('Hello,  Evercode!')
 })
 
