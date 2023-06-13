02e4a71f407de0888d6e1e24b290a7de02408b74
gabrielmanoel-hub <gabrielmanoel1401@outlook.com>
2023-06-13 15:32:04 -0300

    retornando string da função e implementando test

---
+++ __test__/hello.test.js
@@ -0,0 +1,6 @@
+const helloMessage = require('../index')
+
+
+it('text string', () => {
+    expect(helloMessage).toEqual('Hello  Evercode')
+})
--- index.js
+++ index.js
@@ -2,4 +2,5 @@ const hello = require('./hello');
 
 const name = "Evercode";
 const helloMessage = hello(name);
+module.exports = helloMessage
 console.log(helloMessage);
